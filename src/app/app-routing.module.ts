43cafc70a6359cd2c5cc503e920fa95c12307dd3
bhargav <challabhargav99@gmail.com>
2024-06-02 18:52:07 +0530

    audits nd routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {
     path: 'search',
     component: SearchComponent,
-    children: [
+    children: [ 
       { path: 'advance-search', component: SearchPaginationComponent },
     ],
   },
