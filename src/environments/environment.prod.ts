4e81d07ef3b115a30f5b00339ef988d22984bfde
bhargav <challabhargav99@gmail.com>
2025-04-10 21:01:39 +0530

    environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.prod.ts (production)
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://mevendtrk-svc-np.maverick.aa.com/vdt' // Full URL for production
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.ts (development)
+export const environment = {
+    production: false,
+    apiBaseUrl: '/api' // Proxy works here during ng serve
+  };
