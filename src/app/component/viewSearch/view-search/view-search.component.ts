6fe509649539cf8361ecd301dc332617a10ba9f4
bhargav <challabhargav99@gmail.com>
2025-04-29 22:00:22 +0530

    new changs

--- src/app/component/viewSearch/view-search/view-search.component.ts
+++ src/app/component/viewSearch/view-search/view-search.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-search',
   templateUrl: './view-search.component.html',
   styleUrls: ['./view-search.component.css'],
+  providers: [DatePipe]
 })
 export class ViewSearchComponent {
   documentsDetails: any = [];
   docAdded :any;
   detailDeleted: any;
   docMoved:any;
+  searchType!: string;
 
-  constructor(private apiService: ApiService, private route:ActivatedRoute, private router:Router) {}
+  constructor(private apiService: ApiService, private route:ActivatedRoute, private router:Router, private datePipe:DatePipe) {}
 
   ngOnInit() {
     this.viewDocuments();
@@ -22,12 +25,17 @@ export class ViewSearchComponent {
       this.detailDeleted = params['detailDeleted']
       this.docMoved = params['docMoved']
     })
+    this.searchType = this.apiService.searchType;
   }
 
   viewDocuments() {
     this.apiService.viewDocuments().subscribe((data) => {
       console.log(data);     
       this.documentsDetails = data;
+      this.documentsDetails.document.reissueDate 
+      if(this.documentsDetails.document.reissueDate  !== null && this.documentsDetails.document.reissueDate  !== undefined && this.documentsDetails.document.reissueDate  !=='null') {
+        this.documentsDetails.document.reissueDate = this.datePipe.transform(this.documentsDetails.document.reissueDate, 'MM/dd/yyyy')
+        }
       console.log(data);      
     })
   }
